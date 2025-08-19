b4bc0ad73f626446b3fc28c1d5b822f427b45a72
风起深空 <15827139224@163.com>
2025-08-19 21:23:15 +0800

    Add files via upload

---
+++ tree(1)/tree.c
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include"tree.h"
+//前序遍历 根左右
+void preorder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	printf("%c ", root->data);
+	preorder(root->leftchild);
+	preorder(root->rightchild);
+}
+
+//中序遍历 左根右
+void inorder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	inorder(root->leftchild);
+	printf("%c ",root->data);
+	inorder(root->rightchild);
+}
+
+
+//后序遍历 左右根
+void postorder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	postorder(root->leftchild);
+	postorder(root->rightchild);
+	printf("%c ", root->data);
+}
+
+//⼆叉树结点个数
+//二叉树结点个数 = 根节点个数 + 左孩子结点个数 + 右孩子节点个数
+//在此处根节点个数可以确定为1，然后右孩子节点个数 = 根节点个数 + 左孩子结点个数 + 右孩子节点个数，以此类推
+//所以可以递归解决这个问题
+int BinaryTreeSize(BTNode* root)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+
+	return 1 + BinaryTreeSize(root->leftchild) + BinaryTreeSize(root->rightchild);
+}
+
---
+++ tree(1)/tree.h
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+//定义链式结构的二叉树
+typedef char BTDataType;
+typedef struct BinaryTreeNode
+{
+	BTDataType data;
+	struct BinaryTreeNode* leftchild;
+	struct BinaryTreeNode* rightchild;
+}BTNode;
+//前序遍历 根左右
+void preorder(BTNode* root);
+//中序遍历 左根右
+void inorder(BTNode* root);
+//后序遍历 左右根
+void postorder(BTNode* root);
+
+
+// ⼆叉树结点个数
+int BinaryTreeSize(BTNode* root);
+//int BinaryTreeSize(BTNode* root,int* psize);
+
+
